631ddc71e7d7de54b0c9281ec11f51a9d8ec8f74
decentlysizedfish <decentlysizedfish@gmail.com>
2026-01-08 20:40:10 -0500

    Rook class finished

--- Game/Board/Tile.cs
+++ Game/Board/Tile.cs
@@ -3,8 +3,8 @@ namespace CsAndPChess.Game.Board
 {
     public readonly struct Tile
     {
-        public int Rank { get; }  // 1 through 8 (representing '1' through '8')
-        public int File { get; } // 1 through 8 (representing 'a' through 'h')
+        public int Rank { get; }  // 1 through 8 (representing '1' through '8') x
+        public int File { get; } // 1 through 8 (representing 'a' through 'h') y
 
     }
 }
--- Game/Pieces/Rook.cs
+++ Game/Pieces/Rook.cs
@@ -1,18 +1,25 @@
 namespace CsAndPChess.Game.Piece
 {
-    public sealed class Piece : IPiece
+    public sealed class Rook : IPiece
     {
         public White IsWhite { get; }
         public static PieceType Type => PieceType.Rook;
 
         public Tile Position { get; private set; }
 
-        public Piece(White isWhite, Tile startPosition)
+        public Rook(White isWhite, Tile startPosition)
         {
             IsWhite = isWhite;
             Position = startPosition;
         }
 
+        public void MoveTo(Tile newPosition)
+        {
+            Position = newPosition;
+        }
+
         // Move set for 2d array
+        // Rook can move any number of squares along a rank or file
+        // [+x, 0], [-x, 0], [0, +y], [0, -y]
     }
 }
